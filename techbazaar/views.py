7cd82b4f62504d916fd8d46fcd95a55c5671f637
Anirban Sarkar <anirbansarkar2046@gmail.com>
2025-08-23 14:50:14 +0600

    Sorting added in homepage product display

--- techbazaar/views.py
+++ techbazaar/views.py
@@ -2,15 +2,32 @@ from django.shortcuts import render
 from store.models import Product, ReviewRating
 
 def home(request):
-    products = Product.objects.all().filter(is_available=True).order_by('created_date')
+    sort_option = request.GET.get('sort', 'latest')  # default: latest
 
-    # Get the reviews
-    reviews = None
-    for product in products:
-        reviews = ReviewRating.objects.filter(product_id=product.id, status=True)
+    # Determine ordering based on selection
+    if sort_option == 'latest':
+        ordering = '-created_date'
+    elif sort_option == 'oldest':
+        ordering = 'created_date'
+    elif sort_option == 'price_high':
+        ordering = '-price'
+    elif sort_option == 'price_low':
+        ordering = 'price'
+    elif sort_option == 'alpha_az':
+        ordering = 'product_name'
+    elif sort_option == 'alpha_za':
+        ordering = '-product_name'
+    else:
+        ordering = '-created_date'  # fallback
+
+    products = Product.objects.filter(is_available=True).order_by(ordering)
+
+    # Collect reviews for all products
+    reviews = ReviewRating.objects.filter(product_id__in=[p.id for p in products], status=True)
 
     context = {
         'products': products,
         'reviews': reviews,
+        'sort_option': sort_option,
     }
     return render(request, 'home.html', context)
